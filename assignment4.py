107f3d8d14a27f1423fa33fc361f82d6b20e4d88
Angela Ferrer <gelferrero01@gmail.com>
2024-01-12 10:08:01 +0800

    Finalized layout of game page

--- assignment4.py
+++ assignment4.py
@@ -25,8 +25,18 @@ Label (window, image=image,border =0,bg="#FFFFFF").place(x=0, y=-40)
 font1= ("Cooper Black", 120)
 font2= ("Fixedsys",22)
 font3= ("Fixedsys",30)
+font4= ("Fixedsys",50)
+
 
 # Title Page
+
+def start ():
+
+    heading.destroy()
+    subheading.destroy ()
+    start_button.destroy()
+
+    game ()
 
 heading= Label(window, text="GAME TIME!", fg ="#2D3250",bg="#FFFFFF", font=font1) 
 heading.place(x=125, y=140)
@@ -34,6 +44,31 @@ heading.place(x=125, y=140)
 subheading = Label(window, text="Let us find the highest among any number of your choice.", fg ="#2D3250",bg="#FFFFFF", font=font2) 
 subheading.place (x=190, y=340)
 
-start_button=customtkinter.CTkButton(window, text="START", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3").place(x=358, y=280)
+start_button=customtkinter.CTkButton(window, command =start, text="START", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
+start_button.place(x=358, y=280)
+
+# Game Page
+
+def game ():
+
+    instruction = Label(window, text="Enter three numbers on each corresponding box then press calculate.", fg ="#2D3250",bg="#FFFFFF", font=font2) 
+    instruction.place (x=110, y=125)
+
+    first_entry= customtkinter.CTkEntry (window, font=font4, text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=150)
+    first_entry.place(x=110, y=135)
+
+    second_entry= customtkinter.CTkEntry (window, font=font4,  text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=150)
+    second_entry.place(x=340, y=135)
+
+    third_entry= customtkinter.CTkEntry (window, font=font4,  text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=150)
+    third_entry.place(x=570, y=135)
+    calculate_button=customtkinter.CTkButton(window, text="CALCULATE", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
+    calculate_button.place(x=318, y=320)
+
+
+
+
+
+
 
 window.mainloop()
