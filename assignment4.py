58f0308678591ed67cbcccdae8e4a3321d0f6ca4
Angela Ferrer <gelferrero01@gmail.com>
2024-01-12 12:43:50 +0800

    Changes in layout and design since the code was not running properly T^T

--- assignment4.py
+++ assignment4.py
@@ -18,54 +18,90 @@ Label (window, image=image,border =0,bg="#FFFFFF").place(x=0, y=-40)
 
 # Determine fonts to be used. 
 
-font1= ("Cooper Black", 120)
-font2= ("Fixedsys",22)
-font3= ("Fixedsys",30)
-font4= ("Fixedsys",50)
+font1= ("Cooper Black", 70)
+font2= ("Fixedsys",18)
+font3= ("Fixedsys",25)
+font4= ("Fixedsys",30)
+font5= ("Cooper Black", 40)
 
-# Create window's main page/start page. 
+# Determine functions to be used. 
 
-def start ():
+    
 
-    heading.destroy()
-    subheading.destroy ()
-    start_button.destroy()
 
-    game ()
+def entry_validation(): 
+    if any (entry == "" for entry in (first_number_entry.get(), second_number_entry.get(), third_number_entry.get())):
+        messagebox.showerror("Oops!", "Kindly fill out all required fields.")
+        return
+    try:
+        first_number= float(first_number_entry.get())
+        second_number=float(second_number_entry.get())
+        third_number=float(third_number_entry.get())
+    
+    except ValueError:
+        messagebox.showerror("Oops!", "Please enter numerical values only.")
+        return
+    
+    def calculate_highest_number(first_number, second_number, third_number):
+        if first_number> second_number and first_number > third_number:
+            return first_number
+        elif second_number>= first_number and second_number>= third_number:
+            return second_number
+        else:
+            return third_number
+    
+    highest_number = calculate_highest_number(first_number, second_number, third_number)
+    number_text_label.configure(text="The highest number is:", font=('Helvetica', 15))
+    highest_number_label.configure(text=(highest_number), font=('Helvetica', 30, 'bold'), text_color="#D21404")
+
+def try_again():
+    first_number_entry.delete(0, END)
+    second_number_entry.delete(0, END)
+    third_number_entry.delete(0, END)
+    number_text_label.configure(text="")
+    highest_number_label.configure(text="")
+
+number_text_label = customtkinter.CTkLabel(window, text="")
+number_text_label.place(relx=0.5, rely=0.4, anchor=CENTER)
+
+highest_number_label = customtkinter.CTkLabel(window, text="")
+highest_number_label.place(relx=0.5, rely=0.65, anchor=CENTER)
+
+
+
+    
+
+# Create corresponding buttons for each function.
+
+calculate_button=customtkinter.CTkButton(window, command= entry_validation, text="CALCULATE", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
+calculate_button.place(x=360, y=350)
+
+exit_button=customtkinter.CTkButton(window, text="EXIT", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
+exit_button.place(x=590, y=350)
+
+try_again_button=customtkinter.CTkButton(window, command= try_again, text="AGAIN", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
+try_again_button.place(x=135, y=350)
+
+
+# Create window's heading. 
 
 heading= Label(window, text="GAME TIME!", fg ="#2D3250",bg="#FFFFFF", font=font1) 
-heading.place(x=125, y=140)
+heading.place(x=380, y=95)
 
-subheading = Label(window, text="Let us find the largest among any number of your choice.", fg ="#2D3250",bg="#FFFFFF", font=font2) 
-subheading.place (x=190, y=340)
+subheading = Label(window, text="Let us help you find the highest amongst any number of your choice.", fg ="#2D3250",bg="#FFFFFF", font=font2) 
+subheading.place (x=110, y=210)
 
-start_button=customtkinter.CTkButton(window, command =start, text="START", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
-start_button.place(x=358, y=280)
+# Create part where the numbers will be inserted by the user. 
 
-# Create page where the numbers will be inserted by the user. 
+first_number_entry= customtkinter.CTkEntry (window, font=font4, text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=60)
+first_number_entry.place(x=110, y=190)
 
-def game ():
+second_number_entry= customtkinter.CTkEntry (window, font=font4,  text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=60)
+second_number_entry.place(x=340, y=190)
 
-    instruction = Label(window, text="Enter three numbers on each corresponding box then press calculate.", fg ="#2D3250",bg="#FFFFFF", font=font2) 
-    instruction.place (x=110, y=125)
+third_number_entry= customtkinter.CTkEntry (window, font=font4,  text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=60)
+third_number_entry.place(x=570, y=190)
 
-    first_number_entry= customtkinter.CTkEntry (window, font=font4, text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=150)
-    first_number_entry.place(x=110, y=135)
-
-    second_number_entry= customtkinter.CTkEntry (window, font=font4,  text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=150)
-    second_number_entry.place(x=340, y=135)
-
-    third_number_entry= customtkinter.CTkEntry (window, font=font4,  text_color="#2D3250",justify="center", fg_color="#FFFFFF", bg_color="#2D3250", border_width=2, width=180, height=150)
-    third_number_entry.place(x=570, y=135)
-
-    calculate_button=customtkinter.CTkButton(window, text="CALCULATE", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
-    calculate_button.place(x=327, y=320)
-
-    exit_button=customtkinter.CTkButton(window, text="EXIT", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
-    exit_button.place(x=590, y=320)
-
-    try_again_button=customtkinter.CTkButton(window, text="AGAIN", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3")
-    try_again_button.place(x=135, y=320)
 
 
 
