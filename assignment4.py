e0dd8575a97e3e39659efa9f7b1b8d6fab756f8c
Angela Ferrer <gelferrero01@gmail.com>
2024-01-12 08:44:00 +0800

    Finalized theme and title page.

--- assignment4.py
+++ assignment4.py
@@ -10,31 +10,30 @@ from tkinter import messagebox
 # Main Window
 
 window=customtkinter.CTk ()
-window.title("What's At Peak?")
+window.title("Game Time!")
 window.geometry("866x500")
-window.configure(bg="#FFF9C7")
+window.configure(bg="#FFFFFF")
 window.resizable (False, False)
 
 # Background
 
 image = PhotoImage(file="bg.png")
-Label (window, image=image,border =0,bg="#FFF9C7").place(x=0, y=-40)
+Label (window, image=image,border =0,bg="#FFFFFF").place(x=0, y=-40)
 
-# Fonts 
+# Fonts
 
-font1=("Showcard Gothic", 130)
-font2=("Bell MT", 20,'bold')
-font3=("Bell MT", 16,"italic")
+font1= ("Cooper Black", 120)
+font2= ("Fixedsys",22)
+font3= ("Fixedsys",30)
 
-# Title
+# Title Page
 
-title= Label(window, text="SOLVE WITH AYE!", fg="#3B3486", bg="#FFF9C7",font=font1)
+heading= Label(window, text="GAME TIME!", fg ="#2D3250",bg="#FFFFFF", font=font1) 
+heading.place(x=125, y=140)
 
+subheading = Label(window, text="Let us find the highest among any number of your choice.", fg ="#2D3250",bg="#FFFFFF", font=font2) 
+subheading.place (x=190, y=340)
 
-
-
-
-# Frame
-
+start_button=customtkinter.CTkButton(window, text="START", font=font3,fg_color="#FFFFFF", bg_color="#FFFFFF",text_color="#2D3250", border_width=2, border_color="#2D3250", hover_color="#C499F3").place(x=358, y=280)
 
 window.mainloop()
